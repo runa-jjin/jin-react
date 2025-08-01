a59e05835c6c74d9df12b189030efb3e6962d34b
taem <taem@taemui-MacBookAir.local>
2025-08-01 11:14:07 +0900

    day27 router6

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css'
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Link } from 'react-router'
 import Fetch from './fetch/Fetch'
 import Counter from './counter/Counter'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Hello World</h1>} />
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
       <Route path="/about" element={<h1>About</h1>} />
       <Route path="/counter" element={<Counter />} />
       <Route path="/fetch" element={<Fetch />} />
     </Routes>
+    </>
+  )
+}
+
+function Nav() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/fetch">Fetch</Link> | <Link to="/counter">Counter</Link>
+    </nav>
   )
 }
 
