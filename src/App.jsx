18c8aea5f09ddc0ab0d6d7d0de4a5962751e3c5b
taem <taem@taemui-MacBookAir.local>
2025-08-01 10:51:27 +0900

    day27 router3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
 
 function App() {
-  return (<></>)
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Hello World</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
+  )
 }
 
 export default App
